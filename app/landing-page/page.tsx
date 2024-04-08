bc1da2eb86b764f8ae4402391a99117c4f175558
CASTELLI Valerio <valeriocastellipro@gmail.com>
2024-04-08 18:56:21 +0200

    test

--- app/landing-page/page.tsx
+++ app/landing-page/page.tsx
@@ -2,13 +2,14 @@ import Header from "@/components/header/header";
 import HeroSection from "@/components/sections/heroSection";
 import ServiceSection from "@/components/sections/serviceSection";
 import Footer from "@/components/footer/footer";
+
 const LandingPage = () => {
     return (
         <div className="container p-6">
             <Header/>
-            <HeroSection/>
+            {/* <HeroSection/>
             <ServiceSection />
-            <Footer />
+            <Footer /> */}
         </div>
     )
 }
