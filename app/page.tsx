236675cdc15eb61484ba97fa99da85e49c56c7ef
CASTELLI Valerio <valeriocastellipro@gmail.com>
2024-04-08 19:01:51 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 "use client"
 
-import {useState} from "react";
-import {Loader} from "lucide-react";
+import { useState } from "react";
+import { Loader } from "lucide-react";
 import LandingPage from "@/app/landing-page/landing";
 
+import Header from "@/components/header/header";
+import HeroSection from "@/components/sections/heroSection";
+import ServiceSection from "@/components/sections/serviceSection";
+import Footer from "@/components/footer/footer"; s
+
 export default function Home() {
-  const [isLoading, setLoading] = useState(false)
-  return (
-    <>
-      <LandingPage/>
-    </>
-  );
+	const [isLoading, setLoading] = useState(false)
+	return (
+		<>
+			<Header />
+			<HeroSection />
+			<ServiceSection />
+			<Footer />
+		</>
+	);
 }
